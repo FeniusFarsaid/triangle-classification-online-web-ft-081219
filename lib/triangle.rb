a74d2bc50eca94c055e38268fd6163ea57ae4289
David Ingram <david@ingramaviation.com>
2019-09-05 23:32:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
       raise TriangleError
     elsif (@side_a +@side_b <= @side_c) || (@side_a +@side_c <= @side_b) || (@side_b + @side_c <= @side_a)
       raise TriangleError
-    elsif
+    else 
   
   
   
